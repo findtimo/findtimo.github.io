1638eca64eba5913f34f9bb66e81f8be8600b3fd
tkyj <82047680+tkyj@users.noreply.github.com>
2023-06-12 23:29:28 +0800

    Updated scroll snap behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -37,32 +37,31 @@ const App = () => {
           <Navbar />
         </div>
         <div className="scroll-container" ref={scrollContainerRef}>
-
-        <div className="relative w-[auto] h-[200vh] scroll-section flex">
-          <div className='relative w-[100%]'>
-            <div className="scroll-content pt-12 md:pt-0">
-              <Hero />
-              <div className={`sticky right-0 top-0 pt-12 block md:hidden `}>
-                  <CharacterCanvas scrollPass={scrollPass} scrollMax={scrollMax} />
+          <div className="relative w-[auto] h-[200vh] scroll-section flex">
+            <div className='relative w-[100%]'>
+              <div className="scroll-content pt-12 md:pt-0">
+                <Hero />
+                <div className={`sticky right-0 top-0 pt-12 block md:hidden `}>
+                    <CharacterCanvas scrollPass={scrollPass} scrollMax={scrollMax} />
+                </div>
+              </div>
+              <div className="scroll-content">
+                <About />
               </div>
             </div>
-            <div className="scroll-content">
-              <About />
-            </div>
-          </div>
-          <div className='absolute flex justify-end w-[100vw] h-[200vh] z-[-1]'>
-            <div className='sticky w-[60%] h-[auto] '>
-              <div className={`sticky right-0 top-0 pt-12 hidden md:block `}>
-                  <CharacterCanvas scrollPass={scrollPass} scrollMax={scrollMax} />
+            <div className='absolute flex justify-end w-[100vw] h-[200vh] z-[-1]'>
+              <div className='sticky w-[60%] h-[auto] '>
+                <div className={`sticky right-0 top-0 pt-12 hidden md:block `}>
+                    <CharacterCanvas scrollPass={scrollPass} scrollMax={scrollMax} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-          {/* <Experience /> */}
-          <div className="scroll-item">
-            <Works />
-          </div>
-          <div className="scroll-item relative z-0">
+            {/* <Experience /> */}
+            <div className="scroll-item">
+              <Works />
+            </div>
+            <div className="scroll-item relative z-0">
               <Contact />
               <StarsCanvas />
             </div>
